5a0ca0f48b2918b4854d5deac38bf27091f81a9d
TesakProKiller1488 <cmetaha.10@bk.ru>
2024-02-26 11:16:45 +0300

    работа 26.02

--- task2/main.js
+++ task2/main.js
@@ -1,1 +1,20 @@
-allah
+import java.util.Scanner;
+
+public class TemperatureCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Введите температуру: ");
+        int temperature = scanner.nextInt();
+        
+        if (temperature > 30) {
+            System.out.println("Вы можете надеть легкую одежду");
+        } else if (temperature > 20) {
+            System.out.println("Нужно надеть теплую одежду");
+        } else if (temperature < 10) {
+            System.out.println("Сегодня холодно, нужно надевать зимнюю одежду");
+        } else {
+            System.out.println("Температура в пределах нормы");
+        }
+    }
+}
---
+++ task3/app.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+    return a + b;
+  }
+  
+  // Пример использования:
+  console.log(sum(5, 7)); // Выведет 12
+  
